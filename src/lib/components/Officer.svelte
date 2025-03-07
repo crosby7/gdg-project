7e60f3d24d93c8f6595ccd4fb4f5b14b9daaee48
Cameron <camcrosby09@gmail.com>
2025-03-07 18:40:38 -0500

    Begin Officers
    Component Officer - a div with image and text aligned.
    
    Officers page - scaffold of page as it is on Figma.

---
+++ src/lib/components/Officer.svelte
@@ -0,0 +1,48 @@
+<script>
+    let { officerName, officerTitle, officerImage } = $props();
+</script>
+
+<style>
+
+    .officer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 350px;
+        max-width: 600px;
+        background-color: var(--main-color);
+        border-radius: 20px;
+        padding: 0 40px;
+
+
+        img {
+            max-width: 300px;
+            height: auto;
+            border-radius: 50%;
+        }
+
+        .officerText {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            color: var(--secondary-color);
+            h3 {
+                font-size: 20px;
+                margin-bottom: 10px;
+            }
+            p {
+                margin: 0;
+            }
+        }
+    }
+
+</style>
+
+<div class="officer">
+    <img src="{officerImage}" alt="officer" />
+    <div class="officerText">
+        <h3>{officerName}</h3>
+        <p>{officerTitle}</p>
+    </div>
+</div>
---
+++ src/routes/officers/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Officer from '$lib/components/Officer.svelte';
+</script>
+
+<style>
+
+    h2 {
+        text-align: center;
+        color: var(--secondary-color);
+        background-color: var(--main-color);
+        padding: 20px;
+        font-size: 28px;
+    }
+    
+    .officers {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 50% 50%;
+        gap: 20px;
+        padding: 40px;
+    }
+
+</style>
+
+    
+
+<h2>Meet the Officers!</h2>
+
+<section class="officers">
+    <Officer officerName="John Doe" officerTitle="President" officerImage="https://picsum.photos/300" />
+<Officer officerName="Jane Doe" officerTitle="Vice President" officerImage="https://picsum.photos/300" />
+<Officer officerName="John Smith" officerTitle="Treasurer" officerImage="https://picsum.photos/300" />
+<Officer officerName="Jane Smith" officerTitle="Secretary" officerImage="https://picsum.photos/300" />
+</section>
